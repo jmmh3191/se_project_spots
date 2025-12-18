7c3b6a7d14b7c421733c4f2a0c7da1b7637468ef
Jesus <monterojesus911@gmail.com>
2025-12-18 10:45:32 -0800

    spots10stage10 webpack updates/images/font

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
+import "./index.css";
+
 import {
   enableValidation,
   validationConfig,
   resetValidation,
   disableButton,
-} from "./validation.js";
+} from "../scripts/validation.js";
 
 const initialCards = [
   {
